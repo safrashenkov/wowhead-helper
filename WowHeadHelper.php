6165c58ff67041f7c92513fba3f52f5997199ec7
safrashenkov <safrashenkov@gmail.com>
2017-07-28 13:39:28 +0000

    Apply fixes from StyleCI

--- WowHeadHelper.php
+++ WowHeadHelper.php
@@ -1,18 +1,18 @@
 <?php
 
 
-
 class WowHeadHelper
 {
     /**
-     * WowHead locale
+     * WowHead locale.
+     *
      * @var string
      */
-    static $locale = 'ru';
-
+    public static $locale = 'ru';
 
     /**
      * @param $url string url
+     *
      * @return string Request result
      */
     private static function __request($url)
@@ -24,27 +24,29 @@ class WowHeadHelper
 
         $output = curl_exec($ch);
         curl_close($ch);
+
         return $output;
     }
 
     private static function __getMainPage()
     {
-        return self::__request('http://' . self::$locale . '.wowhead.com');
+        return self::__request('http://'.self::$locale.'.wowhead.com');
     }
 
     public static function getAffixes($world = 'EU')
     {
         $page = self::__getMainPage();
         $html = \Sunra\PhpSimple\HtmlDomParser::str_get_html($page);
-        $text = $html->find('div.tiw-region-' . $world, 0);
+        $text = $html->find('div.tiw-region-'.$world, 0);
 
         $affixes = $text->find('table.tiw-group-mythicaffix td.icon-both a');
-        $affix_arr = array();
+        $affix_arr = [];
         $i = 0;
         foreach ($affixes as $affix) {
             $affix->find('img', 0)->outertext = '';
             $affix_arr[$i++] = trim($affix->innertext);
         }
+
         return $affix_arr;
     }
 
@@ -52,7 +54,7 @@ class WowHeadHelper
     {
         $page = self::__getMainPage();
         $html = \Sunra\PhpSimple\HtmlDomParser::str_get_html($page);
-        $text = $html->find('div.tiw-region-' . $world, 0);
+        $text = $html->find('div.tiw-region-'.$world, 0);
 
         $buildings = $text->find('.tiw-group-wrapper-building table');
         $building_arr = [];
@@ -62,6 +64,7 @@ class WowHeadHelper
             $t['count'] = trim($building->find('div.tiw-bs-status-progress span', 0)->innertext);
             $building_arr[] = $t;
         }
+
         return $building_arr;
     }
 
@@ -69,18 +72,18 @@ class WowHeadHelper
     {
         $page = self::__getMainPage();
         $html = \Sunra\PhpSimple\HtmlDomParser::str_get_html($page);
-        $text = $html->find('div.tiw-region-' . $world, 0);
+        $text = $html->find('div.tiw-region-'.$world, 0);
 
         $emissaries = $text->find('table.tiw-group-emissary tr[!class]');
         $emissaries_arr = [];
         foreach ($emissaries as $emissary) {
             $t['time'] = $emissary->find('td.tiw-line-ending-short', 0)->innertext;
             $t['emissary'] = $emissary->find('td.icon-none a', 0)->innertext;
-            $t['href'] = 'http://' . self::$locale . '.wowhead.com' . $emissary->find('td.icon-none a', 0)->href;
+            $t['href'] = 'http://'.self::$locale.'.wowhead.com'.$emissary->find('td.icon-none a', 0)->href;
 
             $emissaries_arr[] = $t;
         }
+
         return $emissaries_arr;
     }
 }
-
